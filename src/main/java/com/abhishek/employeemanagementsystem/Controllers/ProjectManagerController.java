fee8f90d13f7eb04f7e3e30e76485e930b0d3692
abhishekshah5486 <abhishek.shah5486@gmail.com>
2024-05-12 13:09:49 +0530

    Implemented Methods to  ASSIGN and UPDATE Department of  a Team

--- src/main/java/com/abhishek/employeemanagementsystem/Controllers/ProjectManagerController.java
+++ src/main/java/com/abhishek/employeemanagementsystem/Controllers/ProjectManagerController.java
@@ -3,6 +3,7 @@ package com.abhishek.employeemanagementsystem.Controllers;
 import com.abhishek.employeemanagementsystem.Services.ProjectManagerServiceImpl;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,5 +17,5 @@ public class ProjectManagerController {
         this.projectManagerService = projectManagerService;
     }
 
-
+    @PostMapping()
 }
